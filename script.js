261dba9e787e1c324eced073b6229907f0038d10
mrntandooofc <ntandoofc@gmail.com>
2025-07-03 00:45:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,7 @@
 setTimeout(() => {
  document.querySelector('.loading-screen').style.display = 'none';
  document.querySelector('.container').style.display = 'block';
-}, 2000);
-
-// Update loading screen text
-document.querySelector('.loading-screen').innerHTML = `
- <div class="loader"></div>
- <h1>Mr Ntando Ofc</h1>
-`;
+}, 4000); // Increased loading time to 4 seconds
 
 // Add event listener to nav links
 document.querySelectorAll('nav a').forEach((link) => {
